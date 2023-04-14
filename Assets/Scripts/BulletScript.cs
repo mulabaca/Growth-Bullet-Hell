8cc93a209fb56b63f2edfa5f29a86fad8c0e43bb
Reyes <wherismyboard@gmail.com>
2023-04-14 16:40:07 -0600

    bullets collide and don't die  to each other

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -13,12 +13,21 @@ public class BulletScript : MonoBehaviour
 
     private CircleCollider2D hitbox;
 
+    public float lifetime = 1;
+
     private bool collided = false;
+
+    private bool dying = false;
+
+    private SpriteRenderer spriteRenderer;
+
+    private float alphaValue = 1f;
 
     void Start()
     {
         rbBullet = GetComponent<Rigidbody2D>();
         hitbox = GetComponent<CircleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void SetBulletSpeed(float speed)
@@ -41,11 +50,42 @@ public class BulletScript : MonoBehaviour
     {   
         if(!collided){
             rbBullet.velocity =  direction;
+        }else if(dying && Time.time > lifetime){
+            rbBullet.velocity =  Vector3.zero; //stop the bullet
+            hitbox.isTrigger = true; //disable collisions
+            spriteRenderer.sortingOrder = 0; //make them part of the background
+            decreaseOpacity();
+            
+        }else if(dying){
+            rbBullet.velocity = rbBullet.velocity * 0.95f;
         }
     } 
 
     void OnCollisionEnter2D(Collision2D collision){
-        collided = true;
+        if(!collided){
+            collided = true;
+            lifetime = Time.time + lifetime;
+        }
+        if(!collision.gameObject.CompareTag("Bullet")){
+            dying = true;
+        }
+        
+    }
+
+    void decreaseOpacity(){
+        alphaValue -= 0.1f;
+        // get the current color of the sprite
+        Color currentColor = spriteRenderer.color;
+
+        // set the alpha value of the color
+        currentColor.a = alphaValue;
+
+        // set the new color on the SpriteRenderer component
+        spriteRenderer.color = currentColor;
+
+        if (alphaValue <= 0){
+            Destroy(gameObject);
+        }
     }
     
 }
