8120c9f78d7d9c11c0624b7ec6462185b3969755
Kamimorrow <Kamimorrow.fe@gmail.com>
2023-04-16 10:52:16 -0400

    Added a Test Dummy Enemy and a spawner.

---
+++ Assets/Scripts/EnemyScripts/spawnDummy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnDummy : MonoBehaviour
+{
+    public GameObject dummyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("POOF!");
+            GameObject dummy = Instantiate(dummyPrefab, new Vector3(8,0,0), transform.rotation);
+        }
+    }
+}
