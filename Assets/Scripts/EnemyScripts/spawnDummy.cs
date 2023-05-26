73317cbe0aaec08f326b90a95811cb7810636c8d
Reyes <wherismyboard@gmail.com>
2023-05-26 00:46:01 -0600

    spawner spawns at random locations now

--- Assets/Scripts/EnemyScripts/spawnDummy.cs
+++ Assets/Scripts/EnemyScripts/spawnDummy.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class spawnDummy : MonoBehaviour
 {
-    public GameObject dummyPrefab;
+    public GameObject commonSpawn;
+
+    public float randomXRangeLow, randomXRangeHigh;
+    public float randomYRangeLow, randomYRangeHigh;
+    private int count = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +26,27 @@ public class spawnDummy : MonoBehaviour
     {
         if(col.gameObject.tag == "Player")
         {
-            Debug.Log("POOF!");
-            GameObject dummy = Instantiate(dummyPrefab, new Vector3(8,0,0), transform.rotation);
-            //dummy.GetComponent<AIDestination>();
+            float spawnpointx, spawnpointy;
+            
+            for (int i = 0; i < count; i++)
+            {
+
+                //randomize position
+                spawnpointx = Random.Range(randomXRangeLow, randomXRangeHigh);
+                spawnpointy = Random.Range(randomYRangeLow, randomYRangeHigh);
+
+                //randomize direction
+                if(Random.value < 0.5f){
+                    spawnpointx *= -1;
+                }
+                if(Random.value < 0.5f){
+                    spawnpointy *= -1;
+                }
+
+                Debug.Log("POOF!");
+                GameObject dummy = Instantiate(commonSpawn, new Vector3(spawnpointx ,spawnpointy ,0), transform.rotation);
+            }
+            count += 1;
         }
     }
 }
